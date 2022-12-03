a5431df07ebf4ebfb6b749a01ed60d629a95abfd
R NIHARIKA <R NIHARIKA@LAPTOP-UEI19BNK>
2022-12-03 22:07:50 +0530

    commit

---
+++ src/main/java/Junit5331/Junit5331/App.java
@@ -0,0 +1,24 @@
+package Junit5331.Junit5331;
+
+/**
+ * Hello world!
+ *
+ */
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+        
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
---
+++ src/test/java/Junit5331/Junit5331/AppTest.java
@@ -0,0 +1,27 @@
+package Junit5331.Junit5331;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ **/
+public class AppTest 
+{
+    /**
+     * Rigorous Test 🙂
+     */
+    @Test
+    public void test()
+    {
+    	App a=new App();
+    	int actual=a.add(15, 15);
+    	int expected=30;
+    	assertEquals(expected,actual);
+        
+    }
+}
